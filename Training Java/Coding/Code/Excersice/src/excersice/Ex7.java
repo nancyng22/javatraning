594878e33b6d3e41b5e1fa56a6d2564c0681f49a
nancyng22 <nancynguyenfu@gmail.com>
2020-05-26 16:27:06 +0700

    this is new update

--- Training Java/Coding/Code/Excersice/src/excersice/Ex7.java
+++ Training Java/Coding/Code/Excersice/src/excersice/Ex7.java
@@ -5,10 +5,20 @@
  */
 package excersice;
 
+import java.util.Scanner;
+
 /**
  *
  * @author anhnv_tt
  */
+/*
+Viết chương trình liệt kê tất cả các số nguyên tố nhỏ hơn n. 
+Số nguyên dương n được nhập từ bàn phím.
+*/
 public class Ex7 {
-    
+    public static Scanner scanner = new Scanner(System.in); 
+    public 
+            
+            
+            m
 }
